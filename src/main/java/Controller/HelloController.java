37075e2b1d12ec0241eac24ae4dd85d71f637ab1
8-bitLincoln <nazwa@mail.ru>
2015-08-24 12:21:25 +0300

    Add spring lib with all config files. Make simple working request with parametr and catch it

---
+++ src/main/java/Controller/HelloController.java
@@ -0,0 +1,68 @@
+package Controller; /**
+ * Created by user on 23.08.2015.
+ */
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/take_my_coordinate",method = RequestMethod.POST)
+    public String Post(@RequestParam("nick") String nick,
+                       @RequestParam("coordinate_x") String coordinate_x,
+                       @RequestParam("coordinate_y") String coordinate_y){
+        System.out.println("Name = " + nick + "\nx = " + coordinate_x + "\ny = " + coordinate_y);
+        return "hello";
+    }
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String test(){
+        System.out.println("Testing 2 - Send Http POST request");
+        sentPost();
+        return "redirect:/hello";
+    }
+
+    public void sentPost(){
+        try {
+            String url = "http://localhost:8080/take_my_coordinate";
+
+            HttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(url);
+
+            List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+            urlParameters.add(new BasicNameValuePair("nick", "C02G8416DRJM"));
+            urlParameters.add(new BasicNameValuePair("coordinate_x", "322"));
+            urlParameters.add(new BasicNameValuePair("coordinate_y", "422"));
+            post.setEntity(new UrlEncodedFormEntity(urlParameters));
+            HttpResponse response = client.execute(post);
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+
+            System.out.println(result.toString());
+
+        }catch (Exception ex){
+            System.out.println("HelloController " + ex);
+        }
+    }
+}
